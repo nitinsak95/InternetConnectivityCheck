5d4f8e69d2ea51b48243a1f46114769d4bf17e35
affixusimac1 <viveking@outlook.com>
2018-10-10 14:36:47 +0530

    xd

--- InternetConnectivity/ViewController.swift
+++ InternetConnectivity/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import JSSAlertView
+import TTGSnackbar
 
 class ViewController: UIViewController {
 
+    var reachability: Reachability?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.reachability = Reachability.init()
+ 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+  
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            if ((self.reachability!.connection) != .none){
+                
+            }else{
+//                let snackbar = TTGSnackbar(message: "No Internet Connectivity",
+//                                           duration: .middle,
+//                                           actionText: "Retry",
+//                                           actionBlock: { (snackbar) in
+//                                            print("Click action!")
+//                })
+//                snackbar.show()
+                let snackbar = TTGSnackbar(message: "TTGSnackbar !",
+                                           duration: .forever,
+                                           actionText: "Action",
+                                           actionBlock: { (snackbar) in
+                                            print("Click action!")
+                                            // Dismiss manually after 3 seconds
+                                            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(4 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)) {
+                                                snackbar.dismiss()
+                                            }
+                })
+                snackbar.show()
+            }
+        
     }
-
-
+    
+  
 }
+
 
